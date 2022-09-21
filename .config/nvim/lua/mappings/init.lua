dfa3eb6df15b6d7d382ece5287f789aba857806f
morphhyy <imbibek05@gmail.com>
2022-09-21 10:51:18 +0545

    .;

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -2,39 +2,41 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Leader Key
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- NVIM TREE
-map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
-map('n', '<leader>o', ':NvimTreeFocus<CR>', opts)
+map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+map("n", "<leader>o", ":NvimTreeFocus<CR>", opts)
 
 -- Save and Exit
-map('n', '<C-s>', ':w<CR>', opts)
-map('n', '<C-q>', ':q<CR>', opts)
+map("n", "<C-s>", ":w<CR>", opts)
+map("n", "<C-q>", ":q<CR>", opts)
 
 -- Remove Highlight
-map('n', '<leader><CR>', ':noh<CR>', opts)
+map("n", "<leader><CR>", ":noh<CR>", opts)
 
 -- Refresh Nvim Tree
-map('n', '<leader>r', ':NvimTreeRefresh<CR>', opts)
+map("n", "<leader>r", ":NvimTreeRefresh<CR>", opts)
 
 -- Packer Sync
-map('n', '<leader>ps', ':PackerSync<CR>', opts)
+map("n", "<leader>ps", ":PackerSync<CR>", opts)
 -- Source
-map('n', '<leader>ll', ':luafile %<CR>', opts)
+map("n", "<leader>ll", ":luafile %<CR>", opts)
 
 -- Bufferline
-map('n', '<A-Left>', ':bprevious<CR>', opts)
-map('n', '<A-Right>', ':bnext<CR>', opts)
-map('n', '<A-1>', ':BufferLineGoToBuffer 1<CR>', opts)
-map('n', '<A-2>', ':BufferLineGoToBuffer 2<CR>', opts)
-map('n', '<A-3>', ':BufferLineGoToBuffer 3<CR>', opts)
-map('n', '<A-4>', ':BufferLineGoToBuffer 4<CR>', opts)
-map('n', '<A-5>', ':BufferLineGoToBuffer 5<CR>', opts)
-map('n', '<A-6>', ':BufferLineGoToBuffer 6<CR>', opts)
-
+map("n", "<A-Left>", ":bprevious<CR>", opts)
+map("n", "<A-Right>", ":bnext<CR>", opts)
+map("n", "<A-1>", ":BufferLineGoToBuffer 1<CR>", opts)
+map("n", "<A-2>", ":BufferLineGoToBuffer 2<CR>", opts)
+map("n", "<A-3>", ":BufferLineGoToBuffer 3<CR>", opts)
+map("n", "<A-4>", ":BufferLineGoToBuffer 4<CR>", opts)
+map("n", "<A-5>", ":BufferLineGoToBuffer 5<CR>", opts)
+map("n", "<A-6>", ":BufferLineGoToBuffer 6<CR>", opts)
 
 --Telescope
 map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 map("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
 map("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
+-- Commenting
+map("n", "<C-_>", "gcc", opts)
--- .config/nvim/lua/packer-config/init.lua
+++ .config/nvim/lua/packer-config/init.lua
@@ -1,45 +1,51 @@
-return require('packer').startup(function()
-    
-    use 'wbthomason/packer.nvim'
-    use 'navarasu/onedark.nvim'
-    
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons',
-  },
-}
-   
-    use 'akinsho/toggleterm.nvim'
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use("navarasu/onedark.nvim")
 
-    use 'nvim-lualine/lualine.nvim'
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+		},
+	})
 
-    use 'rcarriga/nvim-notify'
+	use("akinsho/toggleterm.nvim")
 
-    use { 'akinsho/bufferline.nvim', tag = "v2.*" }
+	use("nvim-lualine/lualine.nvim")
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+	use("rcarriga/nvim-notify")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} 
-    }
-}
-    use 'jose-elias-alvarez/null-ls.nvim'
+	use({ "akinsho/bufferline.nvim", tag = "v2.*" })
 
-    -- LSP    
-    use {
-        "williamboman/nvim-lsp-installer",
-        "neovim/nvim-lspconfig",
-}
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
-    use 'onsails/lspkind.nvim'
- 
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("jose-elias-alvarez/null-ls.nvim")
+
+	use("lukas-reineke/indent-blankline.nvim")
+
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+
+	-- LSP
+	use({
+		"williamboman/nvim-lsp-installer",
+		"neovim/nvim-lspconfig",
+	})
+	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use("L3MON4D3/LuaSnip") -- Snippets plugin
+	use("onsails/lspkind.nvim")
 end)
-
