5ff1fc7e65eaeedfe4b967172a7486cd2a56a332
morphhyy <imbibek05@gmail.com>
2022-10-18 14:11:54 +0545

    change in nvim_cmp & alacritty

--- .config/nvim/lua/lsp-config/language_server.lua
+++ .config/nvim/lua/lsp-config/language_server.lua
@@ -31,7 +31,7 @@ end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local lsp_flags = {
 	debounce_text_changes = 150,
